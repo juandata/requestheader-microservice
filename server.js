9c826cc4ad8377b8bde3c9eb98ebec98bf727190
Glitch (requestheader-microservice) <none>
2018-05-03 20:11:18 +0000

    🎱🌌 Checkpoint ./server.js:1033623/477

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ var app = express();
 app.use(express.static('public'));
 app.use(function (req, res) {
   var ip = req.headers['x-forwarded-for'].split(',')[0];
+  var lang = req.headers['accept-language'].split(',')[0];
+  var soft = req.headers['user-agent'].split(';');
   console.log(req.headers);
-  console.log(ip);
+  console.log(ip, lang, soft[0], soft[1], soft[2]);
   res.end();
 });
 app.listen(process.env.PORT, function () {
